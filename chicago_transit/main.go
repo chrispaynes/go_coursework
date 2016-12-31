e321f4791d7ffda0b41c903fc6694977762a5b90
chrispaynes <cdpaynes@gmail.com>
2016-12-31 15:51:09 -0600

    Reduces fields on Route and Bus types

--- chicago_transit/main.go
+++ chicago_transit/main.go
@@ -3,54 +3,44 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
 )
 
+// Bus is one of many vehicles that belong to a Route
 type Bus struct {
-	XMLName   xml.Name `xml:"bus"`
-	ID        string   `xml:"id"`
-	Latitude  string   `xml:"lat"`
-	Direction string   `xml:"d"`
+	ID        string `xml:"id"`
+	Latitude  string `xml:"lat"`
+	Direction string `xml:"d"`
 }
 
+// Route is a collection of buses following a similar path at a given time
 type Route struct {
-	XMLName xml.Name `xml:"buses"`
-	Buses   []Bus    `xml:"bus"`
-	Time    string   `xml:"time"`
+	Buses []Bus  `xml:"bus"`
+	Time  string `xml:"time"`
 }
 
 func main() {
 
 }
 
-Unmarshal parses the XML-encoded data and stores the result in the value pointed to by v, which must be an arbitrary struct, slice, or string. Well-formed data that does not fit into v is discarded.
-
-
-
 func connectToRoute(route int) (*http.Response, error) {
 	url := "http://ctabustracker.com/bustime/map/getBusesForRoute.jsp?route=" + strconv.Itoa(route)
 	resp, err := http.Get(url)
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err.Error())
-	}
-  
-	xml_data := Route{}
-
-	routes := xml.Unmarshal([]byte(body), &xml_data)
-	fmt.Println(routes)
-	fmt.Println(&xml_data)
-
-	defer resp.Body.Close()
 
 	if err != nil {
 		fmt.Printf("Did not receive a HTTP response from %v\n", "\""+url+"\"")
 		log.Fatal(err)
 	}
 
+	defer resp.Body.Close()
+
 	return resp, err
+}
+
+func mapToRoute(body string) Route {
+	r := Route{}
+	xml.Unmarshal([]byte(body), &r)
+	return r
 }
