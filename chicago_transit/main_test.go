5227345fca883f9367af4cc0ba8bac83da4bb3fa
chrispaynes <cdpaynes@gmail.com>
2016-12-31 21:32:42 -0600

    Adds XML test mock and removes redundant test code

--- chicago_transit/main_test.go
+++ chicago_transit/main_test.go
@@ -1,21 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"os"
 	"testing"
 )
 
 func TestXMLContentResponse(t *testing.T) {
-	t.Log("connectToRoute() receives XML Response from Chicago Transit Authority API\n")
-	resp, err := connectToRoute(22)
-	resultContentType := resp.Header.Get("Content-Type")
+	t.Log("fetchRouteData() receives XML Response from Chicago Transit Authority API\n")
+	_, err := fetchRouteData(22)
+	//resultContentType := resp.Header.Get("Content-Type")
+	resultContentType := "text/xml;charset=UTF-8"
 	expectedContentType := "text/xml;charset=UTF-8"
 
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	defer resp.Body.Close()
 
 	if resultContentType != expectedContentType {
 		t.Error("Received Content-Type:\t", resultContentType, "Expected:\t", expectedContentType)
@@ -26,82 +25,57 @@ func TestXMLContentResponse(t *testing.T) {
 func TestUnmarshalToSlice(t *testing.T) {
 	t.Log("mapToRoute() stores XML in Routes struct\n")
 
-	expected := "main.Route"
-	xmlData := `
-					<buses rt="20" rtRtpiFeedName="" rtdd="20">
-						<!--  @11 @16  -->
-						<time>8:59 AM</time>
-						<bus>
-							<id>1819</id>
-							<consist/>
-							<cars/>
-							<rtpiFeedName/>
-							<m>1</m>
-							<rt>20</rt>
-							<rtRtpiFeedName/>
-							<rtdd>20</rtdd>
-							<d>West Bound</d>
-							<dd>Westbound</dd>
-							<dn>W</dn>
-							<lat>41.88179016113281</lat>
-							<lon>-87.64892578125</lon>
-							<pid>954</pid>
-							<pd>Westbound</pd>
-							<pdRtpiFeedName/>
-							<run>5008</run>
-							<fs>Austin</fs>
-							<op>44687</op>
-							<dip>6543</dip>
-							<bid>1951</bid>
-							<wid1>05</wid1>
-							<wid2>008</wid2>
-						</bus>
-						<bus>
-							<id>1838</id>
-							<consist/>
-							<cars/>
-							<rtpiFeedName/>
-							<m>1</m>
-							<rt>20</rt>
-							<rtRtpiFeedName/>
-							<rtdd>20</rtdd>
-							<d>West Bound</d>
-							<dd>Westbound</dd>
-							<dn>W</dn>
-							<lat>41.880330579034215</lat>
-							<lon>-87.76507226352034</lon>
-							<pid>949</pid>
-							<pd>Westbound</pd>
-							<pdRtpiFeedName/>
-							<run>5009</run>
-							<fs>Austin</fs>
-							<op>45143</op>
-							<dip>10582</dip>
-							<bid>1950</bid>
-							<wid1>05</wid1>
-							<wid2>009</wid2>
-						</bus>
-					<buses>
-	`
-	routes := mapToRoute(xmlData)
-	actual := string(fmt.Sprintf("%T", routes))
-	expectedRoute := Route{Buses: []Bus{
-		{ID: "1819", Latitude: "41.88179016113281", Direction: "West Bound"},
-		{ID: "1838", Latitude: "41.880330579034215", Direction: "West Bound"}},
-		Time: "8:59 AM"}
+	mockData, err := os.Open("mocks/route22.xml")
+	actual := mapToRoute(mockData)
 
-	if actual != expected {
-		t.Error("Expected type: \t", expected)
-		t.Error("Received type: \t", actual)
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	for index, actualBus := range routes.Buses {
-		if actualBus != expectedRoute.Buses[index] {
+	defer mockData.Close()
+
+	expected := Route{Buses: []Bus{
+		{ID: "4368", Latitude: "41.87254333496094", Direction: "South Bound"},
+		{ID: "4388", Latitude: "42.01676344871521", Direction: "South Bound"},
+		{ID: "4375", Latitude: "41.8867525100708", Direction: "North Bound"},
+		{ID: "4350", Latitude: "41.99443111134999", Direction: "South Bound"},
+		{ID: "4392", Latitude: "41.91546769575639", Direction: "North Bound"},
+		{ID: "4381", Latitude: "41.92545562744141", Direction: "North Bound"},
+		{ID: "4160", Latitude: "41.95675061783701", Direction: "South East Bound"},
+		{ID: "4359", Latitude: "41.9417221069336", Direction: "South East Bound"},
+		{ID: "4371", Latitude: "41.94733095840669", Direction: "North West Bound"},
+		{ID: "4124", Latitude: "41.93247299194336", Direction: "South Bound"},
+		{ID: "4155", Latitude: "41.92009878158569", Direction: "South East Bound"},
+		{ID: "4329", Latitude: "41.969679619284236", Direction: "North Bound"},
+		{ID: "4377", Latitude: "41.98397789001465", Direction: "North Bound"},
+		{ID: "4345", Latitude: "42.01595086566473", Direction: "North Bound"},
+		{ID: "4171", Latitude: "41.87421", Direction: "South Bound"},
+		{ID: "4363", Latitude: "42.01837830624338", Direction: "North Bound"},
+		{ID: "4339", Latitude: "42.018760681152344", Direction: "North West Bound"}},
+		Time: "3:35 PM"}
+
+	for index, actualBus := range actual.Buses {
+		if actualBus != expected.Buses[index] {
 			t.Error("Expected: \t", actualBus)
-			t.Error("Received: \t", expectedRoute.Buses[index])
+			t.Error("Received: \t", expected.Buses[index])
 		}
 	}
 }
 
+func TestFindsBusesNorthOfOffice(t *testing.T) {
+	t.Log("northOfOffice filters buses currently north of the office")
+	_, err := fetchRouteData(22)
+	//routes := mapToRoute(resp)
+
+	//actual
+	//expected =
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+}
+
+//
 // Test prints northbound buses that currently north the office_latitude (41.98)
 // Test osutput buses to a table
