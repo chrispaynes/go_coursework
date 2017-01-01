431b0736a4c35a0abd64b65efdd1b026b0cdeb14
chrispaynes <cdpaynes@gmail.com>
2017-01-01 17:33:42 -0600

    Adds Latitude filter and table creation funcs

--- chicago_transit/main.go
+++ chicago_transit/main.go
@@ -30,19 +30,20 @@ func main() {
 func fetchRouteData(route int) (io.ReadCloser, error) {
 	url := "http://ctabustracker.com/bustime/map/getBusesForRoute.jsp?route=" + strconv.Itoa(route)
 	resp, err := http.Get(url)
+	body := resp.Body
 
 	if err != nil {
 		fmt.Printf("Did not receive a HTTP response from %v\n", "\""+url+"\"")
 		log.Fatal(err)
 	}
 
-	defer resp.Body.Close()
-	return resp.Body, err
+	return body, err
 }
 
 func mapToRoute(resp io.ReadCloser) Route {
 	r := Route{}
 	body, err := ioutil.ReadAll(resp)
+	defer resp.Close()
 
 	if err != nil {
 		log.Fatal(err)
@@ -50,4 +51,39 @@ func mapToRoute(resp io.ReadCloser) Route {
 
 	xml.Unmarshal(body, &r)
 	return r
+}
+
+func createTable(route Route) string {
+	officeLatitude := 41.98
+
+	fmt.Println("\n----------------------------------------------------")
+	fmt.Println("BUSES NORTH OF 41.98 LATITUDE")
+	fmt.Println("----------------------------------------------------")
+	fmt.Println("ID\t Latitude\t\t Direction")
+	//for _, bus := range actual.Buses {
+	for _, bus := range route.Buses {
+		lat, _ := strconv.ParseFloat(bus.Latitude, 64)
+
+		if lat > officeLatitude {
+			fmt.Printf("%v\t %v\t %v\t \n", bus.ID, bus.Latitude, bus.Direction)
+		}
+	}
+	fmt.Println("----------------------------------------------------")
+
+	return ""
+}
+
+func filterNorthOfOffice(buses []Bus) []Bus {
+	var filtered []Bus
+	const officeLatitude = 41.98
+
+	for _, bus := range buses {
+		lat, _ := strconv.ParseFloat(bus.Latitude, 64)
+
+		if lat > officeLatitude {
+			filtered = append(filtered, bus)
+		}
+	}
+
+	return filtered
 }
