2390c3afad14609485dded12dfedc2c1da072284
chrispaynes <cdpaynes@gmail.com>
2017-01-02 16:34:27 -0600

    Adds test for calculating median when given a even number of elements

--- pend_oreille_weather_calc/main_test.go
+++ pend_oreille_weather_calc/main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"testing"
 )
 
@@ -49,6 +51,37 @@ func TestCalcMedianCalculation(t *testing.T) {
 	rows3 := [][]string{[]string{"1", "11"}, []string{"2", "22"}, []string{"3", "33"}}
 	expected3 := 22.0
 	result3 := calcMedian(rows3, 1)
+
+	if result1 != expected1 {
+		t.Error("Received:\t", result1)
+		t.Error("Expected:\t", expected1)
+	}
+
+	if result2 != expected2 {
+		t.Error("Received:\t", result2)
+		t.Error("Expected:\t", expected2)
+	}
+
+	if result3 != expected3 {
+		t.Error("Received:\t", result3)
+		t.Error("Expected:\t", expected3)
+	}
+}
+
+func TestCalcEvenMedianCalculation(t *testing.T) {
+	t.Log("getEvenMedian() returns the correct median when given an even amount of elements")
+
+	n1 := []float64{10, 0}
+	expected1 := 5.0
+	result1 := getEvenMedian(n1)
+
+	n2 := []float64{999, 0, 30, 20, 10, -999}
+	expected2 := 15.0
+	result2 := getEvenMedian(n2)
+
+	n3 := []float64{56.43, 69.56, 99.34, -99.56, 459.3}
+	expected3 := 62.995000000000005
+	result3 := getEvenMedian(n3)
 
 	if result1 != expected1 {
 		t.Error("Received:\t", result1)
