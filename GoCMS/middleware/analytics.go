bc9b9343a7a16e987fe4b016ddd85c9608b63d6d
chrispaynes <cdpaynes@gmail.com>
2017-01-21 01:06:52 -0600

    Writes analytics to log file

--- GoCMS/middleware/analytics.go
+++ GoCMS/middleware/analytics.go
@@ -1,8 +1,10 @@
 package middleware
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 // Add is a variadic function that sums ints
@@ -30,11 +32,23 @@ func (c *Chain) Finally(num int) int {
 	return c.Sum + num
 }
 
-// Next executes the next chain function
-func Next(next http.HandlerFunc) http.Handler {
+// CreateLogger creates a new logger that writes to a file
+// TODO: LOG TO DATABASE
+func CreateLogger(filename string) *log.Logger {
+	file, err := os.OpenFile(filename+".log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+	logger := log.New(file, "", log.Ldate|log.Ltime|log.Lshortfile)
+	return logger
+}
+
+// Time executes the next chain function
+func Time(logger *log.Logger, next http.HandlerFunc) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Before")
+		start := time.Now()
 		next.ServeHTTP(w, r)
-		fmt.Println("After")
+		elapsed := time.Since(start)
+		logger.Println(elapsed)
 	})
 }
--- GoCMS/middleware/cmd/main.go
+++ GoCMS/middleware/cmd/main.go
@@ -11,13 +11,14 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello"))
 }
 func main() {
-	sum := middleware.Add(1, 2, 3)
-	fmt.Println(sum)
+	//sum := middleware.Add(1, 2, 3)
+	//fmt.Println(sum)
 
-	chain := &middleware.Chain{0}
-	sum2 := chain.AddNext(1).AddNext(2).AddNext(3).Finally(6)
-	fmt.Println(sum2)
+	//chain := &middleware.Chain{0}
+	//sum2 := chain.AddNext(1).AddNext(2).AddNext(3).Finally(6)
+	//fmt.Println(sum2)
 
-	http.Handle("/", middleware.Next(hello))
+	logger := middleware.CreateLogger("middleware")
+	http.Handle("/", middleware.Time(logger, hello))
 	http.ListenAndServe(":3000", nil)
 }
